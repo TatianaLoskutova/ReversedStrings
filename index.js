cf40fb907d58c3739d25dabd366cdde2a8eb0b16
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-09 22:03:05 +0300

    second var added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function solution(str){
     return str.split('').reverse().join('');
 }
+
+function solution1(str){
+    let s = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        s += str[i];
+    }
+}
